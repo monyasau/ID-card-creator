d15ee7727bfcfa275a5b2114a919eaecd45a47f3
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-09 14:29:24 +0100

    update

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,21 +1,22 @@
-import { useRef, useEffect } from "react";
+import testImage from "../assets/image.jpg";
 
-export default function Canvas() {
-  const canvasRef = useRef(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvasRef.getContext('2d')
-    // context.fillStyle = 'blue';
-    // context.fillRect(10, 10, 50, 50);
-  }, []);
+import { useEffect, useRef } from "react";
+
+const Canvas = () => {
+
+
   return (
-    <div>
-      <canvas
-        ref={canvasRef}
-        width={400} // Set the width of your canvas
-        height={400} // Set the height of your canvas
-        style={{ border: "1px solid black" }}
-      ></canvas>
-    </div>
+    <>
+      <div className="max-w-screen-xl w-full mx-auto my-20">
+        <canvas
+          ref={canvasRef}
+          width={500}
+          height={300}
+          className="border rounded"
+        />
+      </div>
+    </>
   );
-}
+};
+
+export default Canvas;
