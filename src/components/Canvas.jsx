c275c96a5d79867e749b9c27c3293615b3d605e5
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-09 14:31:04 +0100

    building ui

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -17,7 +17,32 @@ const Canvas = () => {
       gender: "......"
     };
 
-    
+    const image = new Image();
+    image.src = testImage;
+    image.onload = () => {
+      //background color
+      context.fillStyle = "#eeeeee";
+      context.fillRect(0, 0, 500, 300);
+      //image
+      context.drawImage(image, 5, 65, 200, 180);
+      //image border
+      context.beginPath();
+      context.rect(5, 65, 200, 180);
+      context.stroke();
+      //header
+      context.fillStyle = bgTheme;
+      context.fillRect(5, 5, 490, 50);
+      context.beginPath();
+      context.rect(5, 5, 490, 50);
+      context.stroke()
+      //footer
+      context.fillStyle = bgTheme;
+      context.fillRect(5, 255, 490, 40);
+      context.beginPath();
+      context.rect(5, 255, 490, 40);
+      context.stroke()
+
+    };
   }, []);
 
   return (
