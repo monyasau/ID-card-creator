34327cfb432fe81299de1421710815b5099f34d6
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-09 14:30:17 +0100

    added dummy data

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -3,7 +3,22 @@ import testImage from "../assets/image.jpg";
 import { useEffect, useRef } from "react";
 
 const Canvas = () => {
+  const canvasRef = useRef(null);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    let bgTheme = "blue";
+    let textTheme = "white";
+    let FormData = {
+      name: "Olajide olanrewaju",
+      idNo: Math.floor(Math.random() * 10000),
+      validTill: new Date().getFullYear() + Math.floor(Math.random() * 4),
+      gender: "......"
+    };
+
+    
+  }, []);
 
   return (
     <>
