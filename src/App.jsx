5ec4368eb6d92e419da8b434604d06b763760ffa
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-09 14:47:19 +0100

    more ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import TopNav from "./components/TopNav";
+import Form from './components/Form'
 import Canvas from "./components/Canvas";
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
   return (
     <>
       <TopNav />
+      <div className="flex max-w-screen-xl w-full mx-auto mt-[10%]">
       <Canvas />
+      <Form/>
+      </div>
     </>
   );
 }
