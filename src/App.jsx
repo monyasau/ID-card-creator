f0d24b453eb429e54d1cc65d4fc338d5135872e7
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-02-11 12:46:34 +0100

    attempting state sharing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import TopNav from "./components/TopNav";
 import Form  from './components/Form'
 import Canvas from "./components/Canvas";
+import { useState } from "react";
 
 function App() {
-
+  const [userData, setUserData]= useState({
+    name: "John doe",
+    gender: "Male",
+    
+  
+  })
 
   return (
     <>
       <TopNav />
       <div className="flex max-w-screen-xl w-full items-center mx-auto mt-[10%]">
-      <Form/>
-        <Canvas/>
+      <Form userInput={userData} setUserInput={setUserData}/>
+        <Canvas userInput={userData}/>
       </div>
     </>
   );
